16ab22b4fb15ab756f43713820e32ef1e0f1e689
tiagostutz <tiagostutz@bb.com.br>
2016-07-16 23:17:11 -0300

    ajustes das imagens que estavam quebradas

--- app/js/configValues.js
+++ app/js/configValues.js
@@ -1,4 +1,4 @@
 ﻿angular.module("Servidor").value("config", {
-    //baseUrl: "https://servidorapi.azurewebsites.net/api"
-     baseUrl: "http://localhost:4207/api"
+    baseUrl: "https://servidorapi.azurewebsites.net/api"
+    //  baseUrl: "http://localhost:4207/api"
 })
