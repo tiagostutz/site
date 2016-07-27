c61627d33e64dd73b964e8ffa713c74b10bdb69c
tiagostutz <tiagostutz@bb.com.br>
2016-07-27 06:03:14 -0300

    adicionado cache nos requests ao prerender para resolver o timeout de 10s que o crawler do FB derruba

--- src/server.js
+++ src/server.js
@@ -4,12 +4,25 @@ var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var compress       = require('compression');
-var request           = require('request');
+var request        = require('request');
+var redis          = require('redis');
 var app            = express();
 
 port = process.env.PORT || 8001;
 
 var prerender = require('prerender-node').set('prerenderToken', 's7zDfbP07ipE4JZKETcm');
+
+console.log('Conectando ao REDIS');
+var client = redis.createClient(6379,'redis');
+client.set('last_start', new Date().getTime());
+
+client.get('last_start', function(err, result) {
+  console.log("Redis ready.... " + result);
+});
+
+client.on("error", function (err) {
+    console.log("REDIS-Error " + err);
+});
 
 app.use('/*', function(req, res, next) {
     next();
@@ -19,11 +32,22 @@ app.use(express.static(__dirname + '/public/', { maxAge: 864 * 7 /* 1d */ }));  
 
 function serve(baseName) {
   app.use(`/${baseName}*`, function(req, res){
+    var url = "http://service.prerender.io/http://novo.servidor.adv.br"+req.originalUrl;
     if (!prerender.shouldShowPrerenderedPage(req))  {
       res.sendFile(__dirname + "/public/index.html");
     }else{
-      console.log("http://service.prerender.io/http://novo.servidor.adv.br"+req.originalUrl);
-      request("http://service.prerender.io/http://novo.servidor.adv.br"+req.originalUrl).pipe(res);
+      client.get(url, function(err, result) {
+        if (!err && result) {
+            res.status(200).send(result);
+        } else {
+            console.log('Cacheando...');
+            request(url, function(errorSEO, responseSEO, bodySEO) {
+              client.set(url, bodySEO);
+              console.log('Pagina cacheada...');
+              res.status(200).send(bodySEO);
+            });
+        }
+      });
     }
   });
 }
